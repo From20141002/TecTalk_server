2b3876e2435f07bb2ed14950f7a2ba10dc47f6d0
asinta@naver.com <asinta@naver.com>
2015-04-09 16:14:41 +0900

    변수수정 name -> id, client -> customer

--- WEB-INF/src/MainPost.java
+++ WEB-INF/src/MainPost.java
@@ -8,16 +8,16 @@ public class MainPost extends HttpServlet{
  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
    request.setCharacterEncoding("utf-8");
    response.setContentType("text/html;charset=utf-8");
-   String driver_name = request.getParameter("DRIVER_NAME");
-   String client_name = request.getParameter("CLIENT_NAME");
+   String driver_id = request.getParameter("DRIVER_ID");
+   String customer_id = request.getParameter("CUSTOMER_ID");
    String item_name = request.getParameter("ITEM_NAME");
    String address = request.getParameter("ADDRESS");
    PrintWriter out = response.getWriter();
    out.println("<HTML>");
    out.println("<TITLE>Main Informain - 결과 화면</TITLE></HEAD>");
    out.println("<BODY>");
-   out.println("택배기사 고유번호 :  " + driver_name + "<BR>");
-   out.println("고객 고유번호 : " +  client_name + "<BR>");
+   out.println("택배기사 고유번호 :  " + driver_id + "<BR>");
+   out.println("고객 고유번호 : " +  customer_id + "<BR>");
    out.println("상품이름 : " +  item_name + "<BR>");
    out.println("배송지 : " +  address+ "<BR>");
    out.println("저장되었습니다.");
